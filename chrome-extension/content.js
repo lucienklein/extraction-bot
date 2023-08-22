81070afde50fca0ad73046bcab550fe168c8a3f8
lucienklein <lucien.kln@gmail.com>
2023-08-22 17:19:18 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -29,15 +29,26 @@ const addButton = () => {
   if (url.pathname === "/kalilab.php" && title.innerText === "RECHERCHE DE DEMANDE") {
     var iframe = document.getElementById("iframePrincipal");
     var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var table = innerDoc.querySelector(".tableau.tableDemande");
     var rows = innerDoc.querySelectorAll(".tableau.tableDemande .dj");
 
+    var button = document.createElement("button");
+    button.innerHTML = "Click me";
+    button.onclick = function () {
+      console.log("clicked");
+    };
+    table.insertBefore(button, table.firstChild);
+
     rows.forEach((row) => {
-      var button = document.createElement("button");
-      button.innerHTML = "Click me";
-      button.onclick = function () {
-        console.log("clicked");
-      };
-      row.appendChild(button);
+      var checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      row.insertBefore(checkbox, row.firstChild);
+    });
+
+    var otherRows = innerDoc.querySelectorAll(".tableau.tableDemande :not(.dj)");
+    otherRows.forEach((row) => {
+      var emptyTh = document.createElement("th");
+      row.insertBefore(emptyTh, row.firstChild);
     });
   }
 };
