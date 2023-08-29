eb6ccb81230144b28d6547828d8d14c294ae6be4
lucienklein <lucien.kln@gmail.com>
2023-08-29 19:10:52 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -91,21 +91,26 @@ const addButtonToRequest = async () => {
     .match(/idDemande=(\d+)/)[1];
 
   const iframeQuerco = document.createElement("iframe");
-  const originalDocumentWrite = iframeQuerco.contentWindow.document.write;
-  iframeQuerco.contentWindow.document.write = function (content) {
-    if (this.defaultView.parent === window) {
-      console.error("Attempt to modify parent document blocked!");
-      return;
-    }
-    originalDocumentWrite.call(this, content);
-  };
-
   iframeQuerco.setAttribute("id", "iframeQuerco");
   iframeQuerco.setAttribute("src", `${origin}/moduleSil/demande/saisie/index.php?choix=modif&idDemande=${idRequest}`);
   iframeQuerco.setAttribute("style", "width: 100%; height: 100%; border: none;");
   innerDoc.body.appendChild(iframeQuerco);
 
-  await new Promise((resolve) => (iframeQuerco.onload = resolve));
+  await new Promise((resolve) => {
+    iframeQuerco.onload = function () {
+      // Save the original method
+      const originalDocumentWrite = this.contentWindow.document.write;
+
+      this.contentWindow.document.write = function (content) {
+        if (this.defaultView.parent === window) {
+          console.error("Attempt to modify parent document blocked!");
+          return;
+        }
+        originalDocumentWrite.call(this, content);
+      };
+      resolve();
+    };
+  });
   let innerDocQuerco = iframeQuerco.contentDocument || iframeQuerco.contentWindow.document;
 
   const table = innerDoc.querySelector('tr[valign="top"]').parentNode;
