66a5ef5a2843c31686a781e3e059aae3a913207a
lucienklein <lucien.kln@gmail.com>
2023-08-29 19:05:49 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -90,6 +90,11 @@ const addButtonToRequest = async () => {
     .getAttribute("action")
     .match(/idDemande=(\d+)/)[1];
   const iframeQuerco = document.createElement("iframe");
+  iframeQuerco.setAttribute("id", "iframeQuerco");
+  iframeQuerco.setAttribute("src", `${origin}/moduleSil/demande/saisie/index.php?choix=modif&idDemande=${idRequest}`);
+  iframeQuerco.setAttribute("style", "width: 100%; height: 100%; border: none;");
+  innerDoc.body.appendChild(iframeQuerco);
+
   iframeQuerco.contentWindow.document.write = (content) => {
     if (this.defaultView.parent === window) {
       console.error("Attempt to modify parent document blocked!");
@@ -97,10 +102,6 @@ const addButtonToRequest = async () => {
     }
     originalDocumentWrite.call(this, content);
   };
-  iframeQuerco.setAttribute("id", "iframeQuerco");
-  iframeQuerco.setAttribute("src", `${origin}/moduleSil/demande/saisie/index.php?choix=modif&idDemande=${idRequest}`);
-  iframeQuerco.setAttribute("style", "width: 100%; height: 100%; border: none;");
-  innerDoc.body.appendChild(iframeQuerco);
 
   await new Promise((resolve) => (iframeQuerco.onload = resolve));
   let innerDocQuerco = iframeQuerco.contentDocument || iframeQuerco.contentWindow.document;
