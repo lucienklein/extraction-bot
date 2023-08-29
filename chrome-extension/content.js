6ef50b9ffe5612ca703351eba0b6029d2bad3718
lucienklein <lucien.kln@gmail.com>
2023-08-29 23:27:35 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -27,6 +27,7 @@ const createPopupWithIframe = async (origin, idRequest, prescriptionsInfo) => {
 
   popup.alert = function () {};
   popup.document.body.innerHTML = `
+
   <!DOCTYPE html>
 <html lang="fr">
 
@@ -64,6 +65,7 @@ const createPopupWithIframe = async (origin, idRequest, prescriptionsInfo) => {
         }
 
         .popup-content {
+            position: relative;
             background-color: rgba(255, 255, 255, 0.1);
             padding: 50px;
             border-radius: 20px;
@@ -84,47 +86,23 @@ const createPopupWithIframe = async (origin, idRequest, prescriptionsInfo) => {
             font-size: 26px;
         }
 
-        .loading {
-            border: 2px solid rgba(255, 255, 255, 0.3);
-            border-radius: 50%;
-            border-top: 2px solid #555;
-            width: 20px;
-            height: 20px;
+        .loading-bar {
+            position: absolute;
+            top: -5px;
+            right: -5px;
+            bottom: -5px;
+            left: -5px;
+            border: 5px solid rgba(255, 255, 255, 0.3);
+            border-top-color: #555;
+            border-radius: 25px;
             animation: spin 1s linear infinite;
         }
 
-        .steps {
+        .current-step {
             margin-top: 40px;
             color: #444;
-        }
-
-        .step-item {
-            display: flex;
-            align-items: center;
-            margin-bottom: 15px;
-        }
-
-        .circle {
-            width: 20px;
-            height: 20px;
-            border-radius: 50%;
-            border: 2px solid #333;
-            margin-right: 10px;
-        }
-
-        /* Style for SVG icon */
-        svg {
-            width: 20px;
-            height: 20px;
-            margin-right: 10px;
-        }
-
-        .fini {
             text-align: center;
-            margin-top: 20px;
-            font-weight: 700;
-            color: #444;
-            font-size: 28px;
+            font-size: 20px;
         }
     </style>
 </head>
@@ -135,31 +113,15 @@ const createPopupWithIframe = async (origin, idRequest, prescriptionsInfo) => {
             <div class="header">
                 <span class="header-title">Clip - Extraction</span>
             </div>
-            <div class="steps">
-                <div class="step-item">
-                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 32">
-                        <path d="M 16 3 C 8.800781 3 3 8.800781 3 16 C 3 23.199219 8.800781 29 16 29 C 23.199219 29 29 23.199219 29 16 C 29 14.601563 28.8125 13.207031 28.3125 11.90625 L 26.6875 13.5 C 26.886719 14.300781 27 15.101563 27 16 C 27 22.101563 22.101563 27 16 27 C 9.898438 27 5 22.101563 5 16 C 5 9.898438 9.898438 5 16 5 C 19 5 21.695313 6.195313 23.59375 8.09375 L 25 6.6875 C 22.699219 4.386719 19.5 3 16 3 Z M 27.28125 7.28125 L 16 18.5625 L 11.71875 14.28125 L 10.28125 15.71875 L 15.28125 20.71875 L 16 21.40625 L 16.71875 20.71875 L 28.71875 8.71875 Z"></path>
-                    </svg>
-                    <span>Récupération des ordonnances</span>
-                </div>
-                <div class="step-item">
-                    <div class="loading"></div>
-                    <span>Récupération des Informations</span>
-                </div>
-                <div class="step-item">
-                    <div class="circle"></div>
-                    <span>Insertions des informations dans le dossier</span>
-                </div>
-            </div>
-            <div class="fini">
-                Fini !
+            <div class="loading-bar"></div>
+            <div class="current-step">
+                Récupération des Informations
             </div>
         </div>
     </div>
 </body>
 
 </html>
-
   
   `;
 
