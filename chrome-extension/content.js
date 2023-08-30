5fcc0e23e9e0e326e5826ba7fd5c4b5a88c7af78
lucienklein <lucien.kln@gmail.com>
2023-08-30 11:03:50 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -69,7 +69,6 @@ const addButtonToRequest = async () => {
     .match(/idDemande=(\d+)/)[1];
 
   const iframeBlank = document.createElement("iframe");
-  iframeBlank.src = "data:text/html;charset=utf-8,";
   iframeBlank.style.display = "none";
   document.body.appendChild(iframeBlank);
 
