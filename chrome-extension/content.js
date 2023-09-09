561d46e8bff2f5f574fca56745d4c75a909512a7
lucienklein <lucien.kln@gmail.com>
2023-09-09 19:39:47 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -69,65 +69,6 @@ const styleQButton = `
   </style>
 `;
 
-const addButtonToTable = () => {
-  var url = new URL(window.location.href);
-  const title = document.querySelector("#pageTitle");
-
-  if (url.pathname === "/kalilab.php" && title.innerText === "RECHERCHE DE DEMANDE") {
-    var iframe = document.getElementById("iframePrincipal");
-    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-    var rows = innerDoc.querySelectorAll(".tableau.tableDemande .dj");
-
-    rows.forEach((row) => {
-      const dataId = row.getAttribute("data-id");
-      var button = document.createElement("button");
-
-      button.innerHTML = "✨";
-      button.style = "cursor: pointer;";
-
-      button.onclick = function () {
-        button.innerHTML = "🔄";
-        // fetch("http://example.com/dataId", {
-        //   method: "POST",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //   },
-        //   body: JSON.stringify({ dataId: dataId }),
-        // })
-        //   .then((response) => response.json())
-        //   .then((data) => {
-        //     button.innerHTML = "✨";
-        //   })
-        //   .catch((error) => {
-        //     console.error("Error:", error);
-        //   });
-        //     console.error("Error:", error);
-        //   });
-
-        // Create a div for the message
-        var messageDiv = document.createElement("div");
-        messageDiv.style.position = "fixed";
-        messageDiv.style.right = "20px";
-        messageDiv.style.top = "20px";
-        messageDiv.style.padding = "10px";
-        messageDiv.style.backgroundColor = "#4CAF50";
-        messageDiv.style.color = "white";
-        messageDiv.innerHTML = "Ok";
-
-        // Append the message to the body
-        document.body.appendChild(messageDiv);
-
-        // Remove the message after 5 seconds
-        setTimeout(function () {
-          document.body.removeChild(messageDiv);
-        }, 5000);
-      };
-
-      row.appendChild(button);
-    });
-  }
-};
-
 const validateDialog = (doc) => {
   let interval = setInterval(() => {
     const btnsDialog = [...doc.querySelectorAll(`[role="dialog"] button`)];
@@ -302,9 +243,9 @@ const addButtonToRequest = async () => {
 
       if (innerDocQuerco.querySelector("#continuerForm")) openPopupForMoreInfo(idRequest, acts);
 
-      console.log("pending");
+      if (!iframe) return window.location.reload();
+
       iframe.src = `${origin}/moduleSil/demande/client/recherche/visu.php?MUTEX_DEMANDE_DESTROY=${idRequest}&idDemande=${idRequest}&TRACKER_ID=&&pageSrc=searchDemande`;
-      console.log("end");
     }
   };
   document.head.innerHTML += styleQButton;
@@ -313,6 +254,5 @@ const addButtonToRequest = async () => {
 
 // var iframe = document.getElementById("iframePrincipal");
 // iframe.onload = function () {
-// addButtonToTable();
 addButtonToRequest();
 // };
