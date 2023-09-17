d214660b83f3881b4a0d903935ad50136f544fb3
lucienklein <lucien.kln@gmail.com>
2023-09-17 13:01:15 +0200

    up

--- chrome-extension/Resources/dwt.js
+++ chrome-extension/Resources/dwt.js
@@ -76,55 +76,15 @@ let DWTChromeExtension = {
       if (Dynamsoft.Lib.env.bMobile) {
         // this.DWObject.Addon.Camera.scanDocument();
       } else {
-        this.DWObject.SelectSource(
-          function () {
-            DWTChromeExtension.DWObject.OpenSource();
-            DWTChromeExtension.DWObject.AcquireImage();
-          },
-          function () {
-            console.log("SelectSource failed!");
-          }
-        );
+        DWObject.SelectSourceByIndex(0);
+        DWObject.OpenSource();
+        DWObject.IfShowUI = false; // Hide the scanner UI
+        DWObject.PixelType = 2; // Set scan settings as needed (this sets the color mode to color)
+        DWObject.Resolution = 300; // Set the scan resolution to 300 DPI (or other value as needed)
+
+        DWObject.AcquireImage();
       }
     }
-  },
-  edit: function () {
-    if (this.DWObject) {
-      let imageEditor = this.DWObject.Viewer.createImageEditor();
-      imageEditor.show();
-    }
-  },
-  copy: function () {
-    if (this.DWObject) {
-      if (Dynamsoft.Lib.env.bMobile) {
-        this.DWObject.ConvertToBlob(
-          [this.DWObject.CurrentImageIndexInBuffer],
-          Dynamsoft.DWT.EnumDWT_ImageType.IT_PNG,
-          function (result) {
-            DWTChromeExtension.CopyBlobToClipboard(result);
-          },
-          function (errorCode, errorString) {
-            console.log("convert failed");
-            console.log(errorString);
-            alert("Failed");
-          }
-        );
-      } else {
-        this.DWObject.CopyToClipboard(this.DWObject.CurrentImageIndexInBuffer);
-        alert("Copied");
-      }
-    }
-  },
-  CopyBlobToClipboard: function (blob) {
-    var data = [new ClipboardItem({ "image/png": blob })];
-    navigator.clipboard.write(data).then(
-      function () {
-        alert("Copied");
-      },
-      function () {
-        alert("Failed");
-      }
-    );
   },
   save: function () {
     if (this.DWObject) {
@@ -132,22 +92,7 @@ let DWTChromeExtension = {
     }
   },
   initDWT: function () {
-    const status = document.querySelector(".dwt-status");
-    const license = document.getElementById("dwt").getAttribute("license");
-    if (license) {
-      console.log("using license: " + license);
-      Dynamsoft.DWT.ProductKey = license;
-    }
     Dynamsoft.DWT.Containers = [{ ContainerId: "dwtcontrolContainer", Width: 270, Height: 350 }];
-    Dynamsoft.DWT.RegisterEvent("OnWebTwainReady", function () {
-      console.log("ready");
-      status.innerText = "";
-      DWTChromeExtension.DWObject = Dynamsoft.DWT.GetWebTwain("dwtcontrolContainer");
-      DWTChromeExtension.DWObject.Viewer.width = "100%";
-      DWTChromeExtension.DWObject.Viewer.height = "100%";
-      DWTChromeExtension.DWObject.SetViewMode(2, 2);
-    });
-    status.innerText = "Loading...";
     Dynamsoft.DWT.Load();
   },
 };
