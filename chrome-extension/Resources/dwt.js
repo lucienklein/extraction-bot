5b644c8f778ed5e72ff09b27b964c05c26e11495
lucienklein <lucien.kln@gmail.com>
2023-09-17 15:42:49 +0200

    up

--- chrome-extension/Resources/dwt.js
+++ chrome-extension/Resources/dwt.js
@@ -14,13 +14,16 @@ let DWTChromeExtension = {
     this.DWObject.AcquireImage();
     this.DWObject.CloseSource();
 
-    this.DWObject.ConvertToBlob([0], Dynamsoft.DWT.EnumDWT_ImageType.IT_PDF)
-      .then((blob) => {
-        chrome.runtime.sendMessage({ message: "scan_done", pdf: blob });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    this.DWObject.ConvertToBlob(
+      [0],
+      Dynamsoft.DWT.EnumDWT_ImageType.IT_PDF,
+      function (result, indices, type) {
+        chrome.runtime.sendMessage({ message: "scan_done", pdf: result });
+      },
+      function (errorCode, errorString) {
+        console.log(errorString);
+      }
+    );
   },
   initDWT: function () {
     Dynamsoft.DWT.RegisterEvent("OnWebTwainReady", () => {
