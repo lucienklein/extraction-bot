5be9cedeb8fff128213f4349f294daf3f98d8013
lucienklein <lucien.kln@gmail.com>
2023-09-17 18:36:58 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -35,14 +35,11 @@ const addScanToScreen = (data) => {
   const div = document.createElement("div");
   div.innerHTML = `
   <div style="position: relative; width: 100%; height: 100%;">
-    <img id="displayImage" style="width: auto; height: 100vh ; object-fit: contain; position: relative; z-index: 1;">
+    <img id="displayImage" src=`${"data:image/png;base64," + data._content}` style="width: auto; height: 100vh ; object-fit: contain; position: relative; z-index: 1;">
     <div id="displayText" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; background-color: rgba(0, 0, 0, 0.5); color: white; z-index: 2;">
       Extraction en cours
     </div>
   </div>`;
-
-  const img = document.getElementById("displayImage");
-  img.src = "data:image/png;base64," + data._content;
 
   const principalDiv = document.querySelector("#principalContent");
   principalDiv.style.display = "flex";
@@ -92,7 +89,7 @@ window.addEventListener(
     if (!event.data.message || event.data.message !== "scan_done") return;
 
     addScanToScreen(event.data.result);
-    uploadScan(event.data.result);
+    // uploadScan(event.data.result);
   },
   false
 );
