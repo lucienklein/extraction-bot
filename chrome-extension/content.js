0ea2f4d83ee2bb32086e0f68310447a9da2686b1
lucienklein <lucien.kln@gmail.com>
2023-09-18 21:32:12 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -85,7 +85,7 @@ window.addEventListener(
         inputAnalyse.dispatchEvent(enterKeyEvent);
 
         await new Promise((resolve) => {
-          setTimeout(resolve, 500);
+          setTimeout(resolve, 3000);
           if (!inputAnalyse.classList.contains("ui-autocomplete-loading")) resolve();
         });
 
