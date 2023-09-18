a796e91c7fcdddab7bf633c9a1ebeba1489b384b
lucienklein <lucien.kln@gmail.com>
2023-09-18 11:50:28 +0200

    up

--- chrome-extension/Resources/extractFile.js
+++ chrome-extension/Resources/extractFile.js
@@ -3,21 +3,17 @@
     '[style="background-image:url(http://172.30.69.50/images/icoimage-blanc.png);"]'
   );
 
-  console.log(fileScanned);
-
   for (const file of fileScanned) {
     const script = file.nextElementSibling;
     const data = script.innerText;
 
-    console.log(data);
-
     const pattern = /'https?:\/\/[a-zA-Z0-9.-]+\/moduleKalilab\/scan\/visuImage\.php\?[^']+\\'/;
     const result = data.match(pattern);
     console.log(result);
-    if (result || result.length > 0) return;
+    if (!result || result.length <= 0) return;
 
     const url = result[0];
-    console.log(url);
     const response = await fetch(url);
+    console.log(response);
   }
 })();
