882bb3ca25add8ce573ce1d8deb20405c1d4c5b2
lucienklein <lucien.kln@gmail.com>
2023-09-18 15:36:39 +0200

    up

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -83,7 +83,7 @@ const uploadScan = async (data) => {
   fctRefreshPolygon();
 
   const inputAnalyse = document.querySelector("#analyseCodeAjout");
-  var enterKeyEvent = new KeyboardEvent("keydown", { key: "Enter" });
+  const enterKeyEvent = new KeyboardEvent("keydown", { key: "Enter" });
   const acts = response.data.prescriptions.reduce((acc, cur) => [...acc, ...cur.acts], []);
 
   const boxAnalyse = document.querySelector("#ihmBoxAnalyse .ihmCboxContent.ihmCboxvert");
@@ -100,7 +100,7 @@ const uploadScan = async (data) => {
   let actInserted = [...document.querySelectorAll(`.analyseBox`)].map((act) => act.getAttribute("idanalyse"));
   for (const act of acts) {
     inputAnalyse.value = act.code;
-    inputAnalyse.dispatchEvent(eventENTER);
+    inputAnalyse.dispatchEvent(enterKeyEvent);
 
     await new Promise((resolve) => {
       setTimeout(resolve, 100);
