f76e892363f6b234ea583688c13b62f88e232e5c
lucienklein <lucien.kln@gmail.com>
2023-10-02 21:21:34 +0200

    up

--- content.js
+++ content.js
@@ -72,9 +72,12 @@ window.addEventListener(
     if (event.source != window) return;
     if (!event.data.message || event.data.message !== "displayFile") return;
 
+    const existingDiv = document.querySelector("#divQuerco");
+    if (existingDiv) existingDiv.remove();
+
     const div = document.createElement("div");
     div.innerHTML = `
-    <div style="position: relative; width: 100%; height: 100%;">
+    <div style="position: relative; width: 100%; height: 100%;" id="divQuerco">
       <img id="displayImage" src="${event.data.data}" style="width: auto; height: 100vh ; object-fit: contain; position: relative; z-index: 1;">
       <div id="displayText" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; background-color: rgba(0, 0, 0, 0.5); color: white; z-index: 2; font-size: 2rem; font-weight: bold;">
        Extraction en cours...
