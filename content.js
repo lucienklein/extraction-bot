997d8a020be312e6e75047933daeda25880821ff
lucienklein <lucien.kln@gmail.com>
2023-10-08 15:34:32 +0200

    up

--- content.js
+++ content.js
@@ -80,7 +80,7 @@ window.addEventListener(
     <div style="position: relative; width: 100%; height: 100%;" id="divQuerco">
     ${event.data.data.map(
       (image, index) => `
-        <img id="displayImage" docIndex="${index} src="${image} style="width: auto; height: 100vh ; object-fit: contain; position: relative; z-index: 1;">
+        <img id="displayImage" docIndex="${index}" src="${image}" style="width: auto; height: 100vh ; object-fit: contain; position: relative; z-index: 1;">
       `
     )}
       <div id="displayText" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; background-color: rgba(0, 0, 0, 0.5); color: white; z-index: 2; font-size: 2rem; font-weight: bold;">
