6463b0a220acad55854f4f95fc1cc5ea76071afb
lucienklein <lucien.kln@gmail.com>
2023-10-09 15:59:04 +0200

    up

--- src/main.js
+++ src/main.js
@@ -80,8 +80,9 @@ const init = async () => {
   if (!examDiv) return;
 
   const button = document.createElement("button");
+  button.type = "button";
   button.className =
-    "rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+    "rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
   button.innerText = "Extraction Automatique";
   button.addEventListener("click", async (e) => {
     e.preventDefault();
