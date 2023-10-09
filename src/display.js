b75e778d64a0a3a0906e1cc3e9da80e14bc858c6
lucienklein <lucien.kln@gmail.com>
2023-10-09 19:37:31 +0200

    up

--- src/display.js
+++ src/display.js
@@ -11,18 +11,18 @@ const displayFiles = (files) => {
       <div class="z-10 flex justify-between items-center px-1">
         <div class="flex items-center">
           <button id="previousImage" disabled class="px-5 py-2 text-sm text-gray-700 bg-white border rounded-lg gap-x-2">
-            <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
             </svg>
           </button>
           <button id="nextImage" disabled class="px-5 py-2 text-sm text-gray-700 bg-white border rounded-lg gap-x-2">
-            <svg class="w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" style="transform: rotate(180deg);">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
             </svg>
           </button>
         </div>
         <div class="m-0 p-3">Extraction Automatique</div>
-        <button id="closeButton" class="bg-white rounded-md p-2 text-red-400 hover:text-red-500 hover:bg-gray-100">
+        <button id="closeButton" class="bg-white rounded-lg p-2 text-red-400 hover:text-red-500 hover:bg-gray-100 border border-red-400 hover:border-red-500">
           <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
