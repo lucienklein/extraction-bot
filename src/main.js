78c9e62fe419d2c8fa571e02d67ab757a1aa81fe
lucienklein <lucien.kln@gmail.com>
2023-10-10 14:48:59 +0200

    up

--- src/main.js
+++ src/main.js
@@ -79,8 +79,8 @@ const init = async () => {
   const inputAnalyse = document.querySelector("#analyseCodeAjout");
   if (!inputAnalyse) return;
 
-  inputAnalyse.style.display = "flex";
-  inputAnalyse.style.alignItems = "center";
+  inputAnalyse.parentNode.style.display = "flex";
+  inputAnalyse.parentNode.style.alignItems = "center";
   const app = document.createElement("div");
   inputAnalyse.parentNode.insertBefore(app, inputAnalyse.nextSibling);
   ReactDOM.render(<App />, app);
