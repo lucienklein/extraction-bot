724d6f610c9a2e7acd36519c56936f51f380f32e
lucienklein <lucien.kln@gmail.com>
2023-10-10 18:21:46 +0200

    up

--- src/scenes/popup.jsx
+++ src/scenes/popup.jsx
@@ -126,16 +126,7 @@ const Popup = () => {
                 disabled={displayedFile.id === files[0].id}
                 class="px-5 py-2 text-sm text-gray-700 bg-white border rounded-lg gap-x-2"
               >
-                <svg
-                  class="w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                >
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                </svg>
+                Précédent
               </button>
               <button
                 onClick={(e) => {
@@ -149,17 +140,7 @@ const Popup = () => {
                 disabled={displayedFile.id === files[files.length - 1].id}
                 class="px-5 py-2 text-sm text-gray-700 bg-white border rounded-lg gap-x-2"
               >
-                <svg
-                  class="w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  style={{ transform: "rotate(180deg)" }}
-                >
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                </svg>
+                Suivant
               </button>
             </div>
             <div class="m-0 p-3">Extraction Automatique</div>
