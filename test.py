78be513774e8bdea1656843e34274c8b5dce8694
lucienklein <lucien.kln@gmail.com>
2023-10-11 21:37:13 +0200

    up

--- test.py
+++ test.py
@@ -8,45 +8,48 @@ import winshell
 def fetch_and_unzip_to_secure_location():
     # Fetch the ZIP from the API
     url = "https://github.com/lucienklein/extraction-ordo-api/raw/master/dist.zip?download="
-    response = requests.get(url)
 
-    # Ensure the request was successful
-    if response.status_code == 200:
-        # Save the ZIP to a temporary file
-        with open("temp.zip", "wb") as f:
-            f.write(response.content)
+    # Stream the request
+    with requests.get(url, stream=True) as response:
+        # Ensure the request was successful
+        if response.status_code == 200:
+            # Save the ZIP to a temporary file in chunks
+            with open("temp.zip", "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
 
-    # Extract the ZIP
-    with zipfile.ZipFile("temp.zip", 'r') as zip_ref:
-        zip_ref.extractall("temp_folder")
+            # Extract the ZIP
+            with zipfile.ZipFile("temp.zip", 'r') as zip_ref:
+                zip_ref.extractall("temp_folder")
 
-    # Determine the main directory inside the ZIP (ignoring __MACOSX and other unwanted dirs)
-    contents = [item for item in os.listdir(
-        "temp_folder") if item != "__MACOSX"]
-    if len(contents) == 1:
-        main_directory = os.path.join("temp_folder", contents[0])
-    else:
-        main_directory = "temp_folder"
-
-    # Secure location: User's AppData Folder
-    target_directory = os.path.expanduser('~\\AppData\\Local\\YourAppName')
-    if not os.path.exists(target_directory):
-        os.makedirs(target_directory)
-
-    # Move the contents to the desired folder
-    for item in os.listdir(main_directory):
-        source = os.path.join(main_directory, item)
-        destination = os.path.join(target_directory, item)
-        if os.path.exists(destination):
-            if os.path.isdir(destination):
-                shutil.rmtree(destination)
+            # Determine the main directory inside the ZIP (ignoring __MACOSX and other unwanted dirs)
+            contents = [item for item in os.listdir(
+                "temp_folder") if item != "__MACOSX"]
+            if len(contents) == 1:
+                main_directory = os.path.join("temp_folder", contents[0])
             else:
-                os.remove(destination)
-        shutil.move(source, destination)
+                main_directory = "temp_folder"
 
-    # Cleanup: remove temporary ZIP and folder
-    os.remove("temp.zip")
-    shutil.rmtree("temp_folder")
+            # Secure location: User's AppData Folder
+            target_directory = os.path.expanduser(
+                '~\\AppData\\Local\\YourAppName')
+            if not os.path.exists(target_directory):
+                os.makedirs(target_directory)
+
+            # Move the contents to the desired folder
+            for item in os.listdir(main_directory):
+                source = os.path.join(main_directory, item)
+                destination = os.path.join(target_directory, item)
+                if os.path.exists(destination):
+                    if os.path.isdir(destination):
+                        shutil.rmtree(destination)
+                    else:
+                        os.remove(destination)
+                shutil.move(source, destination)
+
+            # Cleanup: remove temporary ZIP and folder
+            os.remove("temp.zip")
+            shutil.rmtree("temp_folder")
 
 
 def add_to_startup():
