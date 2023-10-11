24ea069afd557a2dc776ac39f64e5ab98f67ee42
lucienklein <lucien.kln@gmail.com>
2023-10-11 20:13:24 +0200

    up

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import os
 import time
 import subprocess
+import urllib.request
+import winshell
+
+GIT_INSTALLER_URL = "https://github.com/git-for-windows/git/releases/download/v2.32.0.windows.1/Git-2.32.0-64-bit.exe"
 
 
 def is_git_installed():
@@ -12,9 +16,17 @@ def is_git_installed():
 
 
 def install_git():
-    # You can download and install Git using an installer or a package manager like Chocolatey.
-    # For simplicity, let's use Chocolatey (assuming it's installed)
-    os.system("choco install git -y")
+    installer_path = "git_installer.exe"
+
+    # Download Git installer
+    urllib.request.urlretrieve(GIT_INSTALLER_URL, installer_path)
+
+    # Run the installer silently
+    os.system(
+        f"{installer_path} /SILENT /COMPONENTS=\"icons,ext\\reg\\shellhere,assoc,assoc_sh\"")
+
+    # Optionally, remove the installer after installation
+    os.remove(installer_path)
 
 
 def pull_every_hour(directory):
@@ -27,9 +39,27 @@ def pull_every_hour(directory):
         time.sleep(3600)  # Sleep for 1 hour
 
 
+def add_to_startup():
+    startup_folder = os.path.expanduser(
+        '~\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup')
+    exe_path = os.path.abspath(__file__)
+    shortcut_path = os.path.join(startup_folder, "git_auto_pull.lnk")
+
+    # Check if already added to startup
+    if not os.path.exists(shortcut_path):
+        winshell.CreateShortcut(
+            Path=shortcut_path,
+            Target=exe_path,
+            Icon=(exe_path, 0),
+            Description="Git Auto Pull"
+        )
+
+
 if __name__ == "__main__":
     if not is_git_installed():
         install_git()
+
+    # add_to_startup()
 
     # Assuming the directory is "C:\\my_repo". This can be customized.
     pull_every_hour("C:\\my_repo")
